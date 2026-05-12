e6062579f2e6633859aafbfd3cf3e3eda46590d1
Seda Ahi <92363990+sedaahi@users.noreply.github.com>
2026-05-12 15:27:15 +0300

    Brands section added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Brands from "../components/Brands";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
+      <Brands />
     </div>
   );
 }
