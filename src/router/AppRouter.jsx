ac2a53f0bfc5049871a8835338d467cb04bf231d
Seda Ahi <92363990+sedaahi@users.noreply.github.com>
2026-05-21 10:59:57 +0300

    ScrollToTop component added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,19 +6,18 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "../layout/Header";
 import PageContent from "../layout/PageContent";
 import Footer from "../layout/Footer";
+import ScrollToTop from "./ScrollToTop";
 
 export default function AppRouter() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="w-full overflow-x-hidden">
         <Header />
         <PageContent />
         <Footer />
 
-        <ToastContainer
-          position="top-right"
-          autoClose={2500}
-        />
+        <ToastContainer position="top-right" autoClose={3000} />
       </div>
     </Router>
   );
